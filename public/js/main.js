c437e0cd793b5cb2922c3d6f82b062ce69b89ed5
zZeTraL <lucashbs.pro@gmail.com>
2022-01-28 18:54:56 +0100

    🌠 Update

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,9 @@ window.onload = function() {
     let darkModeButton = document.getElementById("darkMode");
     darkModeButton.addEventListener('click', () => darkMode.onClick())
 }
+
+console.log(document.getElementsByTagName("input"))
+
+for (let elementsByTagNameElement of document.getElementsByTagName("input")) {
+    elementsByTagNameElement.addEventListener("input", template.onTyping);
+}
--- public/js/manager/ColorTemplateManager.js
+++ public/js/manager/ColorTemplateManager.js
@@ -1,6 +1,22 @@
 let template = (function() {
 
+    // Registering all inputs
+    let backgroundColorInput = document.getElementById("main-background-color");
+
+    // Registering all value which can be modified
+
     return {
+        // Getters
+
+        // Setters
+        setValue(){
+
+        },
+
+        // Listeners
+        onTyping(element){
+            console.log("USER IS TYPING FROM AN INPUT")
+        }
 
     }
 
