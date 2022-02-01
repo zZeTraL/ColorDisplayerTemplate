91879d3edcdfa5be5df3ddf7a7002fe9610d64d5
zZeTraL <lucashbs.pro@gmail.com>
2022-02-01 16:36:09 +0100

    🌠 Update - Add Some responsive designs - Add Node.js Server support

--- routes/app.js
+++ routes/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const http = require('http').createServer(app);
 
+app.use(express.static(path.join(__dirname, "..")));
+
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "views", "home", "index.html"));
+})
 
 http.listen(4200, () => {
     console.log("Serveur lancé sur le port: 4200")
